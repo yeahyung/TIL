4a41ce794ab57d1b0adc407005d0f63f3b0e70d1
yeahyung <yhb77888@naver.com>
2023-05-12 11:32:09 +0900

    LeetCode 1929 Concatenation of Array

---
+++ CodingTest/src/main/java/LeetCode/ConcatenationOfArray_1929.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConcatenationOfArray_1929 {
+
+	@Test
+	public void concatenationOfArrayTest() {
+		Assertions.assertArrayEquals(getConcatenation(new int[]{1,2,1}), new int[]{1,2,1,1,2,1});
+		Assertions.assertArrayEquals(getConcatenation(new int[]{1,3,2,1}), new int[]{1,3,2,1,1,3,2,1});
+	}
+
+	public int[] getConcatenation(int[] nums) {
+		int numsLength = nums.length;
+		int[] ans = new int[numsLength * 2];
+
+		for (int i = 0; i < numsLength; i++) {
+			ans[i] = nums[i];
+			ans[i + numsLength] = nums[i];
+		}
+
+		return ans;
+	}
+
+}
