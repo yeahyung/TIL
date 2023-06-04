d5bf6c15333333c6e025c2cdb28210ce5fd4f50d
yeahyung <yhb77888@naver.com>
2023-06-04 16:51:28 +0900

    Leetcode 1929

--- CodingTest/src/main/java/LeetCode/ConcatenationOfArray_1929.java
+++ CodingTest/src/main/java/LeetCode/ConcatenationOfArray_1929.java
@@ -14,7 +14,6 @@ public class ConcatenationOfArray_1929 {
 	public int[] getConcatenation(int[] nums) {
 		int numsLength = nums.length;
 		int[] ans = new int[numsLength * 2];
-
 		for (int i = 0; i < numsLength; i++) {
 			ans[i] = nums[i];
 			ans[i + numsLength] = nums[i];
